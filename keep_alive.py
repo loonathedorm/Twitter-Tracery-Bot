1a27311567a55198b10aed86f16ad181d1a5775d
Loona The Dorm <137514300+loonathedorm@users.noreply.github.com>
2023-07-28 05:06:06 +0000

    Indentation changes

--- keep_alive.py
+++ keep_alive.py
@@ -6,13 +6,13 @@ app = Flask('')
 
 @app.route('/')
 def home():
-  return "Bot Script is running! If the bot still isn't functioning, check replit console for more details."
+    return "Bot Script is running! If the bot still isn't functioning, check replit console for more details."
 
 
 def run():
-  app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080)
 
 
 def keep_alive():
-  t = Thread(target=run)
-  t.start()
+    t = Thread(target=run)
+    t.start()
