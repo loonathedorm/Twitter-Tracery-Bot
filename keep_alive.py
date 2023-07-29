f8c45bbb5c5c4137179d6229ac1f8ba52479caa9
Loona The Dorm <137514300+loonathedorm@users.noreply.github.com>
2023-07-29 05:03:51 +0000

    Cleaning blank lines

--- keep_alive.py
+++ keep_alive.py
@@ -3,15 +3,12 @@ from threading import Thread
 
 app = Flask('')
 
-
 @app.route('/')
 def home():
     return "Bot Script is running! If the bot still isn't functioning, check replit console for more details."
 
-
 def run():
     app.run(host='0.0.0.0', port=8080)
-
 
 def keep_alive():
     t = Thread(target=run, daemon=True)
